daf9ec9ecae34c4362ef0c8c34414777f1c6512b
João Victor Carvalho <joaovictor.crvl@gmail.com>
2025-04-02 21:40:17 -0300

    components

---
+++ buks/src/components/footer/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function header() {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li><a href=""><p>Home</p></a></li>
+                <li><a href=""><p>Sobre</p></a></li>
+                <li><a href=""><p>Produtos</p></a></li>
+                <li><a href=""><p>Contato</p></a></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default header
