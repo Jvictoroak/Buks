5f14d69ecbe28929d20460f4d421d792f762dedd
Alexandre Andrioli Tucci <alexandreatucci@gmail.com>
2025-04-09 21:02:00 -0300

    cadastro criado

--- buks/src/App.tsx
+++ buks/src/App.tsx
@@ -4,11 +4,13 @@ import './assets/modules/css/boot.css';
 import './assets/modules/css/reset.css';
 import Cabecalho from './components/header';
 import Rodape from './components/footer';
+import Cadastro from './components/cadastro';
 
 function App() {
   return (
     <>
       <Cabecalho/>
+      <Cadastro/>
       <Rodape/>
     </>
   );
