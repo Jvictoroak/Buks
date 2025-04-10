1a2b1054a56d44ca1491a4af2d0d83ba2a7d4296
João Victor Carvalho <joaovictor.crvl@gmail.com>
2025-04-10 09:20:37 -0300

    routes e ajustando as pastas

--- buks/src/App.tsx
+++ buks/src/App.tsx
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import './assets/modules/css/boot.css';
 import './assets/modules/css/reset.css';
-import Cabecalho from './components/header';
-import Rodape from './components/footer';
-import Home from './components/home';
-import Cadastro from './components/cadastro';
+import Cabecalho from './components/sections/header';
+import Rodape from './components/sections/footer';
+import Home from './components/paginas/home';
+import Cadastro from './components/paginas/cadastro';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <Cabecalho />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/cadastre-se" element={<Cadastro />} />
       </Routes>
       <Rodape />
     </Router>
---
+++ buks/src/components/sections/header/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './index.css'
+import logo from '../../../assets/img/logo.png'
+import { Link } from 'react-router-dom'; 
+
+
+function Cabecalho() {
+  return (
+    <header className='cabecalho'>
+      <div className="conteudo-1140">
+        <div className="conteudo">
+          <a href='' className="imagem-container logo">
+            <img src={logo} className='imagem'/>
+          </a>
+          <nav className="menu">
+              <ul className='navegacao'>
+                  <li className='  ativo'><Link to='/' className='texto t1 pag'><p>Inicio</p></Link></li>
+                  <li className=''><Link to="/Sobre" className='texto t1 pag'><p>Sobre</p></Link></li>
+                  <li className=''><Link to="/Produtos" className='texto t1 pag'><p>Produtos</p></Link></li>
+                  <li className=''><Link to="/Contato" className='texto t1 pag'><p>Contato</p></Link></li>
+                  <li className=''><Link to="/Cadastre-se" className='texto t1 pag'><p>Cadastre-se</p></Link></li>
+              </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Cabecalho
