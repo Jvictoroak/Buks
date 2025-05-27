bac4a3907a0e6bf63360a077fb9d6eec18a867b7
João Victor Carvalho <joaovictor.crvl@gmail.com>
2025-05-26 21:02:45 -0300

    produto interna

--- buks/src/components/paginas/produto-interna/index.tsx
+++ buks/src/components/paginas/produto-interna/index.tsx
@@ -3,18 +3,38 @@ import './index.css'
 import { useParams } from 'react-router-dom';
 import produtos from '../../../data/produtos.json'
 import { toUrlFriendly } from '../../../utils/utils'
+import { useEffect, useState } from 'react';
 
-
-
+interface Produto {
+  nome: string;
+  preco: number;
+  imagem: string;
+  descricao: string;
+  estoque: number;
+}
 
 function ProdutoInterna() {
+//  const [produtos, setProdutos] = useState([]);
+  const { produtoId } = useParams<{ produtoId: string }>();
+  const [produtos, setProdutos] = useState<Produto[]>([]);
+  const [produto, setProduto] = useState<Produto | undefined>(undefined);
 
+  useEffect(() => {
+    fetch('http://localhost:3001/produtos')
+      .then(response => response.json())
+      .then(data => setProdutos(data))
+      .catch(error => console.error('Erro ao buscar produtos:', error));
+  }, []);
 
-  const { produtoId } = useParams();
+  useEffect(() => {
+    if (produtos.length && produtoId) {
+      const encontrado = produtos.find((p) => toUrlFriendly(p.nome) === produtoId);
+      setProduto(encontrado);
+    }
+  }, [produtos, produtoId]);
 
-  const produto = produtos.find((p) => toUrlFriendly(p.nome) === produtoId);
   if (!produto) {
-    return (<></>);
+    return <></>;
   }
 
   return (
@@ -29,7 +49,7 @@ function ProdutoInterna() {
             <div className="textos">
               <div className="sub-titulo t1 nome"><p>{produto.nome}</p></div>
               <div className="texto t1 descricao"><p>{produto.descricao}</p></div>
-              <div className="texto t2 descricao"><p>{produto.descricao}</p></div>
+              {/* <div className="texto t2 descricao"><p>{produto.descricao}</p></div> */}
               <div className="texto t1 estoque"><p>Estoque: {produto.estoque}</p></div>
               <div className="titulo t1 preco"><p>R$ {produto.preco}</p></div>
               <div className="botao comprar"><p>Comprar</p></div>
